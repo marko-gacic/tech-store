4fec83dc493314d21b4067d02ded29b36a97921a
unknown <poslovnogacic@gmail.com>
2019-10-28 10:35:17 +0100

    commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import Services from "../components/HomePage/Services";
+import Featured from "../components/HomePage/Featured";
 export default function HomePage() {
- return (
-  <>
-   <h1>hello from  homepage</h1>
-  </>
- )
+  return (
+    <>
+      <Hero title="tech gadgets" max="true">
+        <Link to="/products" className="main-link" style={{ margin: "2rem" }}>
+          our products
+        </Link>
+      </Hero>
+      <Services />
+      <Featured />
+    </>
+  );
 }
